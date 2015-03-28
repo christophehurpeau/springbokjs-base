23c9c28a9184f74e0ec4f5ffa3106fc12905a71c
Christophe Hurpeau <christophe@hurpeau.com>
2015-03-28 16:36:39 +0100

    v0.32.1

--- Makefile
+++ Makefile
@@ -1,5 +1,27 @@
+
 publish:
 	npm publish
+	@VERSION=`node -pe "require('./package.json').version"` && \
+	git tag "v$$VERSION" -m "$$VERSION"
+	git push --tags origin HEAD:master
 
-create-tag:
-	git tag v$$(npm info . version) && git push origin v$$(npm info . version)
+define release
+	VERSION=`node -pe "require('./package.json').version"` && \
+	NEXT_VERSION=`semver -i $(1) "$$VERSION"` && \
+	node -e "\
+		var j = require('./package.json');\
+		j.version = \"$$NEXT_VERSION\";\
+		var s = JSON.stringify(j, null, 2);\
+		require('fs').writeFileSync('./package.json', s);" && \
+	git commit -m "v$$NEXT_VERSION" -- package.json
+endef
+
+
+release-patch:
+	@$(call release,patch)
+
+release-minor:
+	@$(call release,minor)
+
+release-major:
+	@$(call release,major)
--- tasks.mk
+++ tasks.mk
@@ -13,19 +13,35 @@ update:
 clean:
 	rm -Rf lib/* public/dist/* public/images/*
 
-watch: clean
-	node_modules/.bin/gulp pre-build --env=$(ENV)
+watch:
 	node_modules/.bin/gulp watch --env=$(ENV)
 
-build:
+pre-build:
 	node_modules/.bin/gulp pre-build --version=$(VERSION) --env=$(ENV)
+
+build: pre-build
 	node_modules/.bin/gulp build --version=$(VERSION) --env=$(ENV)
 
 migrate:
-	node --harmony lib/server/migrate.js
+	node --harmony lib/migrate.js
 
-publish:
-	npm publish
+define release
+	VERSION=`node -pe "require('./package.json').version"` && \
+	NEXT_VERSION=`semver -i $(1) "$$VERSION"` && \
+	node -e "\
+		var j = require('./package.json');\
+		j.version = \"$$NEXT_VERSION\";\
+		var s = JSON.stringify(j, null, 2);\
+		require('fs').writeFileSync('./package.json', s);" && \
+	git commit -m "v$$NEXT_VERSION" -- package.json
+endef
 
-create-tag:
-    git tag v$$(npm info . version) && git push origin v$$(npm info . version)
+
+release-patch:
+	@$(call release,patch)
+
+release-minor:
+	@$(call release,minor)
+
+release-major:
+	@$(call release,major)
